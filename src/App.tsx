632a537938ad18682c790e530b1d96035b834f5c
NguyenVanNhan-09 <nhan09122004@gmail.com>
2024-08-06 16:02:44 +0700

    config base vite react-ts

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+function App() {
+    return (
+        <>
+            <div className='text-3xl text-green-600 flex justify-center items-center font-bold '>
+                Xin chào các bạn đây là base React Typescript
+            </div>
+        </>
+    )
+}
+
+export default App
